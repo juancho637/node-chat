90c80c1045ec85de279bbf8e6d27a9a978f96593
Juancho Garcia <juancho637@gmail.com>
2020-12-09 18:16:32 -0500

    eliminación de espacio

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ const server = require("http").createServer(app);
 server.listen(process.env.PORT, (err) => {
   if (err) throw new Error(err);
 
-  console.log("Server running on port: ", process.env.PORT);
+  console.log("Server running on port:", process.env.PORT);
 });
