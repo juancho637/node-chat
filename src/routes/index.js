b9c5e714bb6eaef475f8aeb40b606f9ebd8084e2
Juancho Garcia <juancho637@gmail.com>
2020-12-09 11:53:56 -0500

    eliminación de error 404 del archivo de rutas, ya que se paso a un middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,4 @@ const app = require("express")();
 
 app.use("/api", require("./api"));
 
-app.all("*", (req, res) => {
-  res.status(404).send({
-    msg: "not found",
-  });
-});
-
 module.exports = app;
